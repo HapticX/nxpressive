a21a289a3c2930a1d3151303aafc71ed2bf2937a
Ethosa <social.ethosa@gmail.com>
2023-02-12 20:43:13 +0700

    add quart, quint and expo in/out/inout easing modes

--- src/core/animatable.nim
+++ src/core/animatable.nim
@@ -2,6 +2,7 @@
   Provides low-level animatable objects
 ]#
 import
+  strformat,
   math
 
 
@@ -10,9 +11,24 @@ type
     value*: T
   Easing* = enum
     LINEAR
-    EASE_IN_OUT_SINE
-    EASE_IN_SINE
-    EASE_OUT_SINE
+    EASE_IN
+    EASE_OUT
+    EASE_IN_OUT
+    QUAD_IN
+    QUAD_OUT
+    QUAD_IN_OUT
+    CUBIC_IN
+    CUBIC_OUT
+    CUBIC_IN_OUT
+    QUART_IN
+    QUART_OUT
+    QUART_IN_OUT
+    QUINT_IN
+    QUINT_OUT
+    QUINT_IN_OUT
+    EXPO_IN
+    EXPO_OUT
+    EXPO_IN_OUT
 
 
 func animatable*[T](val: T): Animatable[T] =
@@ -28,17 +44,78 @@ func tween*[T](a, b: Animatable[T], t: 0f..1f, easing: Easing): Animatable[T] =
   ## - `b` - end position;
   ## - `t` - number between 0 and 1, where 0 is start and 1 is end;
   ## - `easing` - easing mo de
-  case easing:
-  of LINEAR:
-    Animatable[T](value: a.value + t * (b.value - a.value))
-  of EASE_IN_OUT_SINE:
-    Animatable[T](value: a.value + (b.value - a.value) * -(cos(PI * t) - 1f) / 2f)
-  of EASE_IN_SINE:
-    Animatable[T](value: a.value + (b.value - a.value) * 1f - cos((t * PI) / 2f))
-  of EASE_OUT_SINE:
-    Animatable[T](value: a.value + (b.value - a.value) * cos((t * PI) / 2f))
+  let val =
+    case easing:
+    of LINEAR:
+      t
+    of EASE_IN:
+      1f - cos((t * PI) / 2f)
+    of EASE_OUT:
+      cos((t * PI) / 2f)
+    of EASE_IN_OUT:
+      -(cos(PI * t) - 1f) / 2f
+    of QUAD_IN:
+      t * t
+    of QUAD_OUT:
+      1f - (1f - t) * (1f - t)
+    of QUAD_IN_OUT:
+      if t < 0.5f:
+        2 * t * t
+      else:
+        1f - pow(-2f * t + 2f, 2f) / 2f
+    of CUBIC_IN:
+      t * t * t
+    of CUBIC_OUT:
+      1 - pow(1f - t, 3f)
+    of CUBIC_IN_OUT:
+      if t < 0.5f:
+        4f * t * t * t
+      else:
+        1f - pow(-2f * t + 2f, 3f) / 2f
+    of QUART_IN:
+      t * t * t * t * t
+    of QUART_OUT:
+      1 - pow(1f - t, 5f)
+    of QUART_IN_OUT:
+      if t < 0.5f:
+        16f * t * t * t * t * t
+      else:
+        1f - pow(-2f * t + 2f, 5f) / 2f
+    of QUINT_IN:
+      t * t * t * t
+    of QUINT_OUT:
+      1 - pow(1f - t, 4f)
+    of QUINT_IN_OUT:
+      if t < 0.5f:
+        8f * t * t * t * t
+      else:
+        1f - pow(-2f * t + 2f, 4f) / 2f
+    of EXPO_IN:
+      if t == 0f:
+        0f
+      else:
+        pow(2f, 10f * t - 10f)
+    of EXPO_OUT:
+      if t == 1f:
+        1f
+      else:
+        1f - pow(2f, -10f * t)
+    of EXPO_IN_OUT:
+      if t == 0f:
+        0f
+      elif t == 1f:
+        1f
+      elif t < 0.5f:
+        pow(2f, -20f * t + 10f) / 2f
+      else:
+        (2f - pow(2f, -20f * t + 10f)) / 2f
+  Animatable[T](value: a.value + (b.value - a.value) * val)
 
 
 func tween*[T](a: Animatable[T], b: T, t: 0f..1f, easing: Easing): Animatable[T] =
   ## Returns animatable value at `t` position between `a` and `b`.
   tween(a, animatable(b), t, easing)
+
+
+func `$`*[T](a: Animatable[T]): string =
+  fmt"animatable[{a.value}]"
